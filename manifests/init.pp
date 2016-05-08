39ef742a56d303d8a049785d1eb3c27173c3c4c1
Florent Peyraud <florent@tryphon.eu>
2016-05-08 17:59:29 +0200

    add jessie to debian releases

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,18 @@
 class debian {
   if $operatingsystem == "Debian" {
     if $operatingsystemrelease =~ /^[0-9\.]+$/ {
-      if versioncmp($operatingsystemrelease, "7.0") >= 0 {
-        $release = "wheezy"
-      } else {
-        if versioncmp($operatingsystemrelease, "6.0") >= 0 {
-          $release = "squeeze"
+      if versioncmp($operatingsystemrelease, "8.0") >=0 {
+        $release = "jessie"
+      } else {
+        if versioncmp($operatingsystemrelease, "7.0") >= 0 {
+          $release = "wheezy"
         } else {
-          $release = "lenny"
-        } 
+          if versioncmp($operatingsystemrelease, "6.0") >= 0 {
+            $release = "squeeze"
+          } else {
+            $release = "lenny"
+          } 
+        }
       }
     } else {
       $release = $operatingsystemrelease ? {
@@ -17,7 +21,12 @@ class debian {
       }
     }
   }
-  
+ 
+  $jessie = $release ? {
+    "jessie" => true,
+    default  => false
+  }
+ 
   $lenny = $release ? {
     "lenny" => true,
     default => false
